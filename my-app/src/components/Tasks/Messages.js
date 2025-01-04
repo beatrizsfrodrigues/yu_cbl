2a11f3faaf7ddce32b07331a1d86f969adcbe4fe
Beatriz <40210313@esmad.ipp.pt>
2025-01-04 01:45:59 +0000

    feat: implement message handling with local storage and add message functionality

--- my-app/src/components/Tasks/Messages.js
+++ my-app/src/components/Tasks/Messages.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMessages } from "../../redux/messagesSlice";
+import { fetchMessages, addMessage } from "../../redux/messagesSlice";
 import { fetchPresetMessages } from "../../redux/presetMessagesSlice";
 import { X } from "react-feather";
 
@@ -17,6 +17,7 @@ function Messages({ onClose }) {
   const textOptionsRef = useRef(null);
   const [isPresetMessagesOpen, setIsPresetMessagesOpen] = useState(false);
 
+  //* fetch text messages
   useEffect(() => {
     if (messagesStatus === "idle") {
       dispatch(fetchMessages());
@@ -24,70 +25,51 @@ function Messages({ onClose }) {
   }, [messagesStatus, dispatch]);
 
   useEffect(() => {
+    if (messages && messages.length > 0) {
+      const textSpace = textSpaceRef.current;
+      textSpace.scrollTop = textSpace.scrollHeight;
+    }
+  }, [messages]);
+
+  //* fetch preset text messages
+  useEffect(() => {
     if (presetMessagesStatus === "idle") {
       dispatch(fetchPresetMessages());
     }
   }, [presetMessagesStatus, dispatch]);
 
+  //* open and close preset text messages
   const handleOpenPresetMessages = () => {
     setIsPresetMessagesOpen(!isPresetMessagesOpen);
   };
 
-  const addDragScrolling = (element) => {
-    if (!element) return; // Ensure the element is not null
+  function getFormattedDate() {
+    const now = new Date();
 
-    let isDown = false;
-    let startY;
-    let scrollTop;
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
 
-    const handleMouseDown = (e) => {
-      isDown = true;
-      startY = e.pageY - element.offsetTop;
-      scrollTop = element.scrollTop;
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+  }
+
+  //* send a text message
+  const handleAddMessage = (text) => {
+    const message = {
+      id: messages[0].messages.length + 1,
+      senderId: 1,
+      receiverId: 2,
+      message: text,
+      date: getFormattedDate(),
     };
 
-    const handleMouseLeave = () => {
-      isDown = false;
-    };
-
-    const handleMouseUp = () => {
-      isDown = false;
-    };
-
-    const handleMouseMove = (e) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const y = e.pageY - element.offsetTop;
-      const walk = (y - startY) * 2; // Adjust scroll speed
-      element.scrollTop = scrollTop - walk;
-    };
-
-    element.addEventListener("mousedown", handleMouseDown);
-    element.addEventListener("mouseleave", handleMouseLeave);
-    element.addEventListener("mouseup", handleMouseUp);
-    element.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      element.removeEventListener("mousedown", handleMouseDown);
-      element.removeEventListener("mouseleave", handleMouseLeave);
-      element.removeEventListener("mouseup", handleMouseUp);
-      element.removeEventListener("mousemove", handleMouseMove);
-    };
+    dispatch(addMessage({ message }));
   };
 
-  useEffect(() => {
-    const textSpace = textSpaceRef.current;
-    const textOptions = textOptionsRef.current;
-
-    const removeTextSpaceListeners = addDragScrolling(textSpace);
-    const removeTextOptionsListeners = addDragScrolling(textOptions);
-
-    return () => {
-      if (removeTextSpaceListeners) removeTextSpaceListeners();
-      if (removeTextOptionsListeners) removeTextOptionsListeners();
-    };
-  }, [isPresetMessagesOpen]);
-
+  //* text messages status info
   if (messagesStatus === "loading") {
     return <div>Loading...</div>;
   }
@@ -96,28 +78,40 @@ function Messages({ onClose }) {
     return <div>Error: {error}</div>;
   }
 
+  //* text messages
   let messageContent;
   if (messages && messages.length > 0) {
-    messageContent = messages[0].messages.map((message, index) => {
+    const sortedMessages = [...messages[0].messages].sort(
+      (a, b) => +a.date - +b.date
+    );
+    console.log(sortedMessages);
+    messageContent = sortedMessages.map((message, index) => {
+      const year = message.date.slice(0, 4);
+      const month = message.date.slice(4, 6);
+      const day = message.date.slice(6, 8);
+      const hours = message.date.slice(8, 10);
+      const minutes = message.date.slice(10, 12);
+
       if (message.senderId === 1) {
         return (
           <div key={index} className="textMessage">
             <p className="bubble bubbleBlue">{message.message}</p>
-            <p className="dateText textRight">{message.date}</p>
+
+            <p className="dateText textRight">{`${day}/${month}/${year} ${hours}:${minutes}`}</p>
           </div>
         );
       } else if (message.senderId === "app") {
         return (
           <div key={index} className="textMessage">
             <p className="bubble bubbleDotted">{message.message}</p>
-            <p className="dateText textLeft">{message.date}</p>
+            <p className="dateText textLeft">{`${day}/${month}/${year} ${hours}:${minutes}`}</p>
           </div>
         );
       } else {
         return (
           <div key={index} className="textMessage">
             <p className="bubble">{message.message}</p>
-            <p className="dateText textLeft">{message.date}</p>
+            <p className="dateText textLeft">{`${day}/${month}/${year} ${hours}:${minutes}`}</p>
           </div>
         );
       }
@@ -126,12 +120,17 @@ function Messages({ onClose }) {
     messageContent = <div>Não existem mensagens</div>;
   }
 
+  //* preset text messages column 1
   let presetMsgs;
   if (presetMessages && presetMessages.length > 0) {
     presetMsgs = presetMessages.map((message, index) => {
       if (index % 2 === 0) {
         return (
-          <button key={index} className="optionText">
+          <button
+            key={index}
+            className="optionText"
+            onClick={() => handleAddMessage(message.message)}
+          >
             {message.message}
           </button>
         );
@@ -142,12 +141,17 @@ function Messages({ onClose }) {
     presetMsgs = <div>Não existem mensagens</div>;
   }
 
+  //* preset text messages column 2
   let presetMsgs2;
   if (presetMessages && presetMessages.length > 0) {
     presetMsgs2 = presetMessages.map((message, index) => {
       if (index % 2 !== 0) {
         return (
-          <button key={index} className="optionText">
+          <button
+            key={index}
+            className="optionText"
+            onClick={() => handleAddMessage(message.message)}
+          >
             {message.message}
           </button>
         );
@@ -158,6 +162,7 @@ function Messages({ onClose }) {
     presetMsgs2 = <div>Não existem mensagens</div>;
   }
 
+  //* modal
   return (
     <div className="modal">
       <div className="window">
--- my-app/src/redux/messagesSlice.js
+++ my-app/src/redux/messagesSlice.js
@@ -3,12 +3,18 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchMessages = createAsyncThunk(
   "messages/fetchMessages",
   async () => {
+    const localData = localStorage.getItem("messages");
+    if (localData) {
+      return JSON.parse(localData);
+    }
+
     const response = await fetch("/messages.json");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     try {
       const data = await response.json();
+      localStorage.setItem("messages", JSON.stringify(data));
       return data;
     } catch (error) {
       throw new Error("Failed to parse JSON");
@@ -19,11 +25,34 @@ export const fetchMessages = createAsyncThunk(
 const messagesSlice = createSlice({
   name: "messages",
   initialState: {
-    data: null,
+    data: [],
     status: "idle",
     error: null,
   },
-  reducers: {},
+  reducers: {
+    addMessage: (state, action) => {
+      const { message } = action.payload;
+      const conversation = state.data.find(
+        (conv) =>
+          JSON.stringify(conv.usersId) ===
+          JSON.stringify([message.senderId, message.receiverId])
+      );
+
+      if (conversation) {
+        conversation.messages.push(message);
+      } else {
+        state.data.push({
+          id: state.data.length + 1,
+          senderId: message.senderId,
+          receiverId: message.receiverId,
+          messages: message,
+          date: new Date().toLocaleString(),
+        });
+      }
+
+      localStorage.setItem("messages", JSON.stringify(state.data));
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchMessages.pending, (state) => {
@@ -40,4 +69,5 @@ const messagesSlice = createSlice({
   },
 });
 
+export const { addMessage } = messagesSlice.actions;
 export default messagesSlice.reducer;
