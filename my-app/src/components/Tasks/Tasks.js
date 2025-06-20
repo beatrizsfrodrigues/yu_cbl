55fca7b6834b3edbdcb49e9ead1f4ee504e8df90
MartaCaetano <marta.caetano@ua.pt>
2025-06-20 20:03:49 +0100

    not tarefas 6

--- my-app/src/components/Tasks/Tasks.js
+++ my-app/src/components/Tasks/Tasks.js
@@ -364,7 +364,11 @@ function Tasks() {
     }
   );
 
-  const hasTaskNotification = tasks.some((task) => task.notification === true);
+  const hasTaskNotification = tasks.some(
+    (task) => task.notification === true && task.verified === false //&&
+    //task.assigned === true && // tarefa do parceiro
+    //task.completed === true // marcada como concluída
+  );
 
   return (
     <div className="mainBody" id="tasksBody">
@@ -386,12 +390,13 @@ function Tasks() {
           Recebidas
         </button>
         <span className="divider">|</span>
-        {hasTaskNotification && <span className="badge"></span>}
         <button
+          style={{ position: "relative" }}
           className={`filter-button ${filter === "assigned" ? "active" : ""}`}
           onClick={() => handleFilterChange("assigned")}
         >
           Atribuídas
+          {hasTaskNotification && <span className="badge"></span>}
         </button>
       </div>
       <div id="tasksSpace">
